fd0f51946e6c7949a0ab5e07b4d33061f00014da
johncline07 <clinejm@protonmail.com>
2026-09-16 19:39:06 -0400

    Configure Azure remote Terraform backend

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatejohncline07"
+    container_name       = "tfstate"
+    key                  = "hub-spoke-lab.tfstate"
+    use_azuread_auth     = true
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
